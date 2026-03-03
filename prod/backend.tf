5cadcf2e1edc631ac4c1bd5d83d194a176fc0030
Shivam Sharma <118384998+Shivamssharma03@users.noreply.github.com>
2026-03-03 10:25:30 +0530

    Fix S3 bucket name in backend configuration

--- prod/backend.tf
+++ prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "shivamrtcbackends"
+    bucket         = "shivamrtcbackendss"
     key            = "rtcinfrastructure/prod/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "rtcbackendtable"
